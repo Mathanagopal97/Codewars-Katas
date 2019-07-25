d8cc8ebed9932fbce14248e1c385c61b2f6dcd09
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-07-25 20:24:59 +0530

    File Upload

---
+++ IKEA.java
@@ -0,0 +1,23 @@
+/*
+
+One day you decide to go to IKEA to buy yourself a couple of tables for your new house, and you want to make sure that you get enough screws to successfully assemble and put together all of your newly-bought tables. Let's also assume that for every table you buy, the next table will be bigger than the previous one, thus requiring one more screw. For example, table number 1 requires 1 screw, table number 2 requires 2 screws and so forth.
+
+Make a boolean method called checkEnoughScrews with an input of total screws and tables that returns True if there are enough screws to assemble the tables, or return False otherwise.
+*/
+
+public class IKEA {
+    public static boolean checkEnoughScrews(int tables, int screws) {
+      int sum=0;
+      for(int i=1;i<=tables;i++){
+        sum=sum+i;
+      }
+      if(sum<=screws){ 
+          return true;
+        }
+      return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkEnoughScrews(6, 20));
+    }
+  }
---
+++ NeedleInHaystack.java
@@ -0,0 +1,39 @@
+/*
+Find something in an Array
+
+Create a find function that takes a string and an array as arguments. If the string is in the array, return true.
+
+For example:
+
+NeedleInHaystack.find("hello", new String[]{"bye bye", "hello"})  // return true
+// the haystack (searchable array) will never be null, but could contain null values;
+NeedleInHaystack.find("be", new String[]{"careful", null, "oops"})  // return false
+// also the needle could be null too
+NeedleInHaystack.find(null, new String[]{"watch", "for", null})  // return true
+Note: "Hello" !== "hello" // case-sensitive comparison
+*/
+
+public class NeedleInHaystack {
+    public static boolean isNullOrEmpty(String str) {
+       if(str != null && !str.isEmpty())
+          return false;
+       return true;
+    }
+ 
+   public static boolean find(String needle, String[] haystack) {
+     for(int i=0;i<haystack.length;i++){
+          if(isNullOrEmpty(haystack[i]) && isNullOrEmpty(needle)){
+             return true;
+          }
+          else if(!isNullOrEmpty(haystack[i]) && !isNullOrEmpty(needle)){
+            if(haystack[i].equals(needle))
+              return true;
+          }
+       }
+       return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(find("hello", new String[]{"bye bye", "hello"}));
+    }
+ }
